3d5dc3a826cb080fb0949306cffaaa6260b34392
widya yasa <widyarusmananda15@gmail.com>
2023-02-12 11:34:45 +0800

    curd jenis

--- app/controllers/Jenis.php
+++ app/controllers/Jenis.php
@@ -5,10 +5,28 @@ class jenis extends Controller{
         if(empty($_SESSION['user'])){
             redirect('auth/');
         }
+        $data['total_jenis'] = $this->model('jenisModel')->selectAllJenis();
         $data['title'] = 'Category';
         $this->view('templates/header', $data);
         $this->view('templates/side-top-nav', $data);
         $this->view('pages/jenis', $data);
         $this->view('templates/footer', $data);
+    }
+
+    public function add(){
+        if($this->model('jenisModel')->addJenis($_POST)){
+            redirect('jenis');
+        }
+    }
+    public function edit(){
+        if($this->model('jenisModel')->editJenis($_POST)){
+            redirect('jenis');
+        }
+        redirect('jenis');
+    }
+    public function delete(){
+        if($this->model('jenisModel')->deleteJenis($_POST['id_jenis'])){
+            redirect('jenis');
+        }
     }
 }
--- app/models/jenisModel.php
+++ app/models/jenisModel.php
@@ -15,4 +15,36 @@ class jenisModel{
         $this->db->query("SELECT * FROM {$this->jenis}");
         return $this->db->resultSet();
     }
+
+    public function addJenis($data)
+    {
+        $query = "INSERT INTO {$this->jenis} VALUES (NULL, :nama_jenis, :kode_jenis,  :keterangan)";
+        $this->db->query($query);
+        $this->db->bind('nama_jenis', $data['nama_jenis']);
+        $this->db->bind('kode_jenis', $data['kode_jenis']);
+        $this->db->bind('keterangan', $data['keterangan']);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
+    public function editJenis($data)
+    {
+        $query = "update {$this->jenis} set `nama_jenis`=:nama_jenis, `kode_jenis`=:kode_jenis, `keterangan`=:keterangan where id_jenis=:id_jenis" ;
+        $this->db->query($query);
+        $this->db->bind('id_jenis', $data['id_jenis']);
+        $this->db->bind('nama_jenis', $data['nama_jenis']);
+        $this->db->bind('kode_jenis', $data['kode_jenis']);
+        $this->db->bind('keterangan', $data['keterangan']);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
+    public function deleteJenis($id)
+    {
+        $query = "delete from {$this->jenis} where `id_jenis`=:id_jenis";
+        $this->db->query($query);
+        $this->db->bind('id_jenis', $id);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
 }
